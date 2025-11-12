fd040d7cfa9415838eee8125f67e4f81b275eabd
Deva <shravankumardevasani@gmail.com>
2025-11-12 19:11:27 +0530

    Added Navbar.js

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>
+                Home
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
